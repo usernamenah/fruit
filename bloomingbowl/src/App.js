600dde6ec1791eee4dcee935a01ab8a39fdcd0c4
P.Sujith <sujithpappala777@gmail.com>
2025-03-21 14:19:03 +0530

    added  pjhotos

--- bloomingbowl/src/App.js
+++ bloomingbowl/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Homepage from "./components/Homepage1.js";
+// import FruitBowl from './components/FruitBowl1.js';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <Homepage/>
+       {/* <FruitBowl/> */}
        
       </header>
     </div>
